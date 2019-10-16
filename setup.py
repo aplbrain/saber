a1a544f668d97e77e2a350da7a3706fd82dbc94f
Raphael Norman-Tenazas <Raphael.Norman-Tenazas@jhuapl.edu>
2019-10-16 10:27:37 -0400

    Corrected use of setup.py -- includes requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-
+deps = [
+            "watchdog(==0.9.0)",
+            "parse(==1.9.0)",
+            "boto3(==1.9.79)",
+            "docker(==3.7.0)",
+            "datajoint(==0.11.3)",
+            "cwltool(==1.0.20181217162649)",
+      ]
 setup(name='conduit',
       version='1.0',
       description='Conduit tool for SABER',
@@ -9,6 +16,8 @@ setup(name='conduit',
       author_email='raphael.norman-tenazas@jhuapl.edu',
       url='https://github.com/aplbrain/saber',
       packages=['conduit', 'conduit.utils'],
-
+      scripts=['conduit/conduit'],
+      install_requires=deps,
+      setup_requires=deps
 
      )
