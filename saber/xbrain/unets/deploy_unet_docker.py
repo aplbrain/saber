52574c2712a736a89a0928d49f6b8c1dc5439bc4
Ubuntu <ubuntu@ip-172-31-29-188.ec2.internal>
2020-02-27 16:53:49 +0000

    changes classify f1

--- saber/xbrain/unets/deploy_unet_docker.py
+++ saber/xbrain/unets/deploy_unet_docker.py
@@ -72,9 +72,9 @@ if __name__ == '__main__':
     np.save(args.output, y_hat)
 
     if args.lbl_file:
-        y_true = np.load(args.lbl_file) # X, Y, Z
-        y_true = np.transpose(y_true)
+        y_hat = np.transpose(np.squeeze(y_hat)) # X,Y,Z
+        y_true = np.load(args.lbl_file) # X,Y,Z
+        print("Output data has shape: {}".format(y_hat.shape))
         print('Groundtruth data has shape: {}'.format(y_true.shape))
-        y_true = y_true[:, np.newaxis, :, :].astype(np.float32) #Z, chan, Y, X
         f1 = f1_score(y_true, y_hat)
         print("F1: {}".format(f1))
